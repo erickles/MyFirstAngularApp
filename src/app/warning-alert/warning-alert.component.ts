75971cc3d9d0e39b8fe551b84f325d6e31ab52f3
Erick Souza <erick842006@gmail.com>
2021-01-03 13:16:06 -0300

    first commit

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-warning-alert',
+    template: `
+        <div class="alert alert-warning">
+            <strong>Warning!</strong> Indicates a warning that might need attention.
+        </div>
+    `
+})
+export class WarningAlertComponent {
+
+
+
+}
